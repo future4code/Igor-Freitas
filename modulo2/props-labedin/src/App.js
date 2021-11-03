10f3d50057f5314994b35ad0b79768e4a858c3af
0dayig0r <igormedeiros.f@gmail.com>
2021-11-03 18:24:24 -0300

    React1

---
+++ modulo2/props-labedin/src/App.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+import CardPequeno from './components/CardPequeno/CardPequeno'
+import styled, {createGlobalStyle}  from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+`
+
+const AppFlex = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 30px;
+`
+
+const PageSectionContainer = styled.div`
+  width: 40vw;
+  margin: 10px 0;
+`
+const H2 = styled.h2`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+
+`
+
+function App() {
+  return (
+    <div>
+    <AppFlex>
+      <PageSectionContainer>
+        <H2>Dados pessoais</H2>
+        <CardGrande 
+          imagem="https://i.postimg.cc/q7cFkw2S/foto.png" 
+          nome="Igor Medeiros" 
+          descricao="Sou aluno do curso de Desenvolvedor Full Stack na Labenu. Não tenho experiência formal no âmbito da T.I"
+        />
+        
+        <ImagemButton 
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+          texto="Ver mais"
+        />
+      </PageSectionContainer>
+
+      <CardPequeno/>
+
+      <div className="page-section-container">
+        <H2>Experiências profissionais</H2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Labenu" 
+          descricao="Aluno no curso de DEV FULL STACK" 
+        />
+        
+        <CardGrande 
+          imagem="https://logodownload.org/wp-content/uploads/2019/03/nasa-logo-6.png" 
+          nome="NASA" 
+          descricao="Apontando defeitos." 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+    </AppFlex>
+    </div>
+  );
+}
+
+export default App;
