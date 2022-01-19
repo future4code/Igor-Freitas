a2bc69a9e7a143f0148ca968f658596bdb48b1f7
0dayIg0r <igormedeiros.f@gmail.com>
2022-01-19 18:29:47 -0300

    Typescript

---
+++ modulo4/intro-typescrip/build/Atividade2/Atividade.js
@@ -0,0 +1,9 @@
+"use strict";
+function imprimeTresCoresFavoritas() {
+    const cor1 = ("Insira sua primeira cor favorita");
+    const cor2 = ("Insira sua segunda cor favorita");
+    const cor3 = ("Insira sua terceira cor favorita");
+    console.log(['Azul', 'Preto', 'Cinza']);
+}
+imprimeTresCoresFavoritas();
+//# sourceMappingURL=Atividade.js.map
