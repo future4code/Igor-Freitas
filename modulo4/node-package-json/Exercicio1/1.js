6a1cb41e338fe37cddb9f414f07900fd593ff111
0dayIg0r <igormedeiros.f@gmail.com>
2022-01-18 21:17:37 -0300

    add desafios

--- modulo4/node-package-json/Exercicio1/1.js
+++ modulo4/node-package-json/Exercicio1/1.js
@@ -1,15 +1,31 @@
 // a. Usando o process.argv
 
 // b.
+let red, purple, reset;
+red   = '\u001b[31m';
+purple  = '\u001b[34m';
+reset = '\u001b[0m';
+
 const userName = process.argv[2]
 const yearsOld = Number(process.argv[3])
-
 const message = `Olá ${userName}, Você têm ${yearsOld} anos`
 
-console.log(message)
+if(userName && yearsOld ){
+    console.log(purple + message)
+} else{
+    console.log(red + 'Esperava-se 2 parametros e só foi recebido um')
+}
+
+
+
+
 
 
 // 
 const sumYears = yearsOld + 7
 const sumMessage = `Olá ${userName}, Você têm ${yearsOld} anos. E em sete anos você terá ${sumYears}.`
-console.log(sumMessage)
+if(sumYears && sumMessage ){
+    console.log(purple + sumMessage)
+} else{
+    console.log(red + 'Esperava-se 2 parametros e só foi recebido 1')
+}
