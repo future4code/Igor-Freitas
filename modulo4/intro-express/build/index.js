f30cd06a6d80b5b55a114ff329053223016bb168
0dayIg0r <igormedeiros.f@gmail.com>
2022-01-24 18:18:02 -0300

    building

--- modulo4/intro-express/build/index.js
+++ modulo4/intro-express/build/index.js
@@ -48,8 +48,45 @@ const users = [
 app.get('/users', (req, res) => {
     const currentUsers = users;
     const getUsers = currentUsers.map((user) => {
-        return user.username;
+        return user;
     });
     const result = getUsers;
     res.status(200).send(result);
+});
+const post = [
+    {
+        id: 1,
+        userId: 1,
+        title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+        body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+    },
+    {
+        id: 1,
+        userId: 2,
+        title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
+        body: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut',
+    },
+    {
+        id: 3,
+        userId: 3,
+        title: 'magnam facilis autem',
+        body: 'ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae',
+    }
+];
+app.get('/posts', (req, res) => {
+    const currentPosts = post;
+    const getPosts = currentPosts.map((posts) => {
+        return posts;
+    });
+    const result = getPosts;
+    res.status(201).send(result);
+});
+app.get('/posts/:id', (req, res) => {
+    const userId = Number(req.params.id);
+    const postUser = post.filter((p) => {
+        if (userId == p.userId) {
+            return p;
+        }
+    });
+    res.status(200).send(postUser);
 });
