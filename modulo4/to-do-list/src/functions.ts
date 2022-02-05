7e06724edaf2a33168ea765750c7f9014dd1e583
0dayIg0r <igormedeiros.f@gmail.com>
2022-02-05 15:49:02 -0300

    principais ends

--- modulo4/to-do-list/src/functions.ts
+++ modulo4/to-do-list/src/functions.ts
@@ -1,34 +1,48 @@
 import { connection } from "./connection";
-import nextId, { setPrefix } from "react-id-generator";
 
-export const getUsers = async (tableName: string): Promise<any> =>{
+// ver todos  users
+export const getUsers = async (tableName: string): Promise<any> => {
     const result = await connection()
-    .select('*')
-    .from(tableName)
+        .select('*')
+        .from(tableName)
     return result
 }
 
-export const getUser = async (id: any): Promise<any> =>{
-    const result = await connection()
-    .select('*')
-    .from('ListToDoListUser')
-    .where('id', id)
+
+// ver pelo id 
+export const getUser = async (id: any): Promise<any> => {
+    const result = await connection().select('*').from('TodoListUser').where('id', id)
     return result
 }
-    setPrefix('ID')
-    const ID = nextId()
-    export const createUser = async (name:string,  nickname:string, email:string): Promise<any> =>{
+
+
+// criar user
+export const createUser = async (name: string, nickname: string, email: string): Promise<any> => {
     await connection.raw(`
-        INSERT INTO ToDoListUser (id, name, nickname, email)
-        VALUES("${ID}", "${name}", "${nickname}", "${email}")
+        INSERT INTO TodoListUser (id, name, nickname, email)
+        VALUES("${Date.now()}", "${name}", "${nickname}", "${email}")
     `)
 }
 
-export const updateUser = async (id:string, name:string, nickname:string): Promise<any> =>{
-    await connection("ListToDoListUser")
-    .update({
-        name: name,
-        nickname: nickname
-    })
-    .where("id", id)
+// att user
+export const updateUser = async (id: string, name: string, nickname: string): Promise<any> => {
+    await connection("TodoListUser")
+        .update({
+            name: name,
+            nickname: nickname
+        })
+        .where("id", id)
+}
+// criar task
+export const tasks =  async (title:string, description:string, limitDate:string, creatorUserId:string) =>{
+    await connection.raw(`
+        INSERT INTO TodoListTask (id, title, description, limit_date, creator_user_id)
+        VALUES("${Date.now()}","${title}", "${description}", "${limitDate}", "${creatorUserId}")
+    `)
+}
+
+// pegar task por id
+export const getTask = async (id:string) =>{
+    const result = await connection("TodoListTask").select("*").from("TodoListTask").where("id", id)
+    return result
 }
--- modulo4/to-do-list/src/index.ts
+++ modulo4/to-do-list/src/index.ts
@@ -1,4 +1,4 @@
-import { connection } from "./connection";
+
 import express, { Express, Response, Request } from "express";
 import cors from 'cors'
 
@@ -8,66 +8,117 @@ app.use(express.json())
 app.use(cors())
 
 import { AddressInfo } from "net";
-import { send } from "process";
-import { getUnit } from "@mui/material/styles/cssUtils";
-import { getUser } from "./functions";
+import { getUser, createUser, updateUser, tasks, getTask } from "./functions";
 
-const server = app.listen(process.env.PORT || 3006, ()=>{
-    if(server){
+const server = app.listen(process.env.PORT || 3006, () => {
+    if (server) {
         const address = server.address() as AddressInfo
         console.log(`Server is running in http://localhost:${address.port}`)
-    } else{
+    } else {
         console.error(`failure upon stargin server.`)
     }
 })
 
 let reqError = 0 // LET PARA TRATAMENTO DE ERRO 
+// app.get('/ping', (req:Request, res:Response)=>{
+//     res.send('Pong')
+// })
+
 
 // create user
-app.post('/users', async(req:Request, res:Response)=>{
-    try{
-        const {name, username, email} = req.body
-        
-        if(!name || !username || !email){
+app.post('/users', async (req: Request, res: Response) => {
+    try {
+        const { name, nickname, email } = req.body
+
+        if (!name || !nickname || !email) {
             reqError = 401
-            res.status(reqError).send('.')
+            res.status(reqError).send('Fill all the fields')
         }
-        
-    } catch(err:any){
+        await createUser(name, nickname, email)
+
+        res.status(200).send('Created')
+
+    } catch (err: any) {
         reqError = 500
         res.status(reqError).send('Try again.')
     }
+
 })
 
 // pegar user
-app.get('/user/:id', async (req:Request, res:Response)=>{
-    try{
-        const id:string = req.params.id
+app.get('/user/:id', async (req: Request, res: Response) => {
+    try {
+        const id: string = req.params.id
         const user = await getUser(id)
 
-        if(user.lenght === 0){
+        if (user.length === 0) {
             reqError = 404
             res.status(reqError).send(`User aren't found`)
         }
         res.status(200).send(user)
-    } catch(err:any){
+    } catch (err: any) {
         res.status(500).send(err.message)
     }
-    
+
 })
+
+
 // editar user
-app.put('user/edit/:id', async (req:Request, res:Response)=>{
-    try{
-        const id:string = req.params.id
-        const {name, nickname} = req.body
-        
-        if(!name || !nickname){
+app.put('/user/edit/:id', async (req: Request, res: Response) => {
+    try {
+        const id: string = req.params.id
+        const { name, nickname } = req.body
+        await updateUser(id, name, nickname)
+        if (!name || !nickname) {
             reqError = 401
             res.status(reqError).send()
         }
         res.status(201).send('Completed')
 
-    } catch(err:any){
+    } catch (err: any) {
         res.status(500).send(err.message)
+    }
+})
+
+// Add task
+app.post('/task', async (req: Request, res: Response) => {
+    try {
+        const { title, description, limitDate, creatorUserId } = req.body
+
+        const dateLimit = limitDate.split("/")
+
+        const limit = `${dateLimit[2]}/${dateLimit[1]}/${dateLimit[0]}` // DATA FORMATADA PRO MYSQL
+
+        await tasks(title, description, limit, creatorUserId)
+
+        if (!title || !description || !limitDate || !creatorUserId) {
+            reqError = 401
+            res.status(401).send('Fill all the fields')
+        }
+        res.status(200).send('Added task')
+    } catch (err: any) {
+        res.status(500).send(err.message)
+    }
+})
+
+
+
+// pegar task por id
+app.get('/task/:id', async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
+        const task = await getTask(id)
+
+        if (!id) {
+            reqError = 401
+            res.status(reqError).send('Please, insert an ID')
+        }
+        if (task.length === 0) {
+            reqError = 401
+            res.status(401).send('Please, insert a valid ID')
+        }
+        res.status(200).send(task)
+    } catch (err: any) {
+        res.status(500).send('Try again')
     }
 })
