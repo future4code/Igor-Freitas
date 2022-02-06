6c8bc84723b3e6f0491050131560ff2ba6871755
0dayIg0r <igormedeiros.f@gmail.com>
2022-02-06 19:49:17 -0300

    add até atividade 18

--- modulo4/to-do-list/src/functions.ts
+++ modulo4/to-do-list/src/functions.ts
@@ -43,10 +43,24 @@ export const tasks = async (title: string, description: string, limitDate: strin
     `)
 }
 
+
 // pegar task por id
 export const getTask = async (id: string) => {
     const result = await connection("TodoListTask").select("*").from("TodoListTask").where("id", id)
     return result
+}
+
+// atribuir user resposnavel pela task
+export const responsible = async (taskID: string, userResponsibleID: string) => {
+    const result = await connection.raw(`
+    INSERT INTO TodoListResponsibleUserTaskRelation (task_id, responsible_user_id)
+    VALUES(
+        "${taskID}",
+        "${userResponsibleID}"
+    );
+        `)
+    return result
+
 }
 
 
@@ -57,7 +71,24 @@ export const getAllUsers = async () => {
 }
 
 
-// pegar tarefas do user x ou y 
+// pegar responsavel pela task
+
+export const getResponsibleUser = async (id: string) => {
+    const result = await connection()
+        .select('*')
+        .from('TodoListResponsibleUserTaskRelation')
+    return result
+}
+
+// pegar task por id
+export const getTaskByID = async (id: string) => {
+    const result = await connection()
+        .select('*')
+        .from('TodoListTask ')
+    return result
+}
+
+// pegar task do user x ou y 
 export const getTaskByUserId = async (creatorUseId: string) => {
     const result = await connection.raw(`
     SELECT t.id as taskId, title, description, limit_date as limitDate, status, creator_user_id as creatorUserId, u.nickname as creatorUserNickName FROM TodoListTask t
@@ -66,18 +97,44 @@ export const getTaskByUserId = async (creatorUseId: string) => {
     return result
 }
 
+// atualizar task
+export const attTask = async (id: string) => {
+    const result = await connection.raw(`
+    UPDATE TodoListTask
+    SET status = "doing"
+    WHERE id = "${id}";
+    `)
+    return result
+}
 
-// // atribuir user resposnavel pela task
-// export const responsible = async (user: string) => {
-//     const result = await connection.raw(`
-//         SELECT task_id as task, FROM TodoListTask t
-//         INNER JOIN TodoListUser u ON u.id = creator_user_id WHERE creator_user_id = "${user}"
-//     `)
-//     return result
-// }
+// pegar task por status
+export const getTaskByStatus = async (status: string) => {
+    const result = await connection.raw(`
+        SELECT * FROM TodoListTask WHERE status = "${status}"
+    `)
+    return result
+}
+
+// pegar task atrasada
+export const delayedTask = async () => {
+    const result = await connection.raw(`
+        SELECT t.id as taskId, title, description, limit_date as limitDate, status, creator_user_id as creatorUserId, u.nickname as creatorUserNickName FROM TodoListTask t
+        INNER JOIN TodoListUser u ON u.id = creator_user_id 
+        `)
+    return result
+}
 
 // pesquisar user 
 export const searchUser = async (user: string) => {
     const result = await connection().select("*").from("TodoListUser").where("nickname", user)
     return result
+}
+
+export const searchTask = async (srch: string): Promise<any> => {
+    const result = await connection.raw(`
+    SELECT TodoListTask.id, title, description, limit_date, status, creator_user_id, nickname FROM TodoListTask
+    LEFT JOIN TodoListUser ON TodoListTask.creator_user_id = TodoListUser.id WHERE TodoListTask.description like '%${srch}%' 
+    OR TodoListTask.title like '%${srch}%'
+    `)
+    return result[0]
 }
--- modulo4/to-do-list/src/index.ts
+++ modulo4/to-do-list/src/index.ts
@@ -1,4 +1,3 @@
-
 import express, { Express, Response, Request } from "express";
 import cors from 'cors'
 
@@ -8,7 +7,24 @@ app.use(express.json())
 app.use(cors())
 
 import { AddressInfo } from "net";
-import { getUser, createUser, updateUser, tasks, getTask, getAllUsers, getTaskByUserId, searchUser, responsible } from "./functions";
+
+import {
+    createUser,
+    updateUser,
+    tasks,
+    getTask,
+    getAllUsers,
+    getTaskByUserId,
+    searchUser,
+    responsible,
+    getResponsibleUser,
+    getTaskByID,
+    attTask,
+    getTaskByStatus,
+    delayedTask,
+    searchTask,
+} from "./functions";
+
 
 const server = app.listen(process.env.PORT || 3006, () => {
     if (server) {
@@ -46,13 +62,17 @@ app.post('/users', async (req: Request, res: Response) => {
 })
 
 // pegar todos users
-app.get('/user/all', async (req:Request, res:Response)=>{
-    try{
+app.get('/user/all', async (req: Request, res: Response) => {
+    try {
         const result = await getAllUsers()
+        if (result.length === 0) {
+            reqError = 404
+            res.status(reqError).send('Zero users on Database')
+        }
         res.status(200).send(result)
-    } catch(err:any){
+    } catch (err: any) {
         res.status(500).send(err.message)
-    } 
+    }
 })
 
 
@@ -119,53 +139,202 @@ app.get('/task/:id', async (req: Request, res: Response) => {
 
 
 // pegar tarefas por id 
-app.get('/task', async (req:Request, res:Response)=>{
-       try{
+app.get('/task', async (req: Request, res: Response) => {
+    try {
         const creatorUseId = req.query.creatorUseId as string
 
         const result = await getTaskByUserId(creatorUseId)
 
-        res.status(201).send(result)        
-       } catch(err:any){
+        res.status(201).send(result)
+    } catch (err: any) {
         res.status(500).send(err.message)
-       }
+    }
 })
 
-// responstavel pela task
-app.post('/task/responsible', async (req:Request, res:Response)=>{
-    try{
-        const user = req.body.user as string
-        const result = await responsible(user)
+// responstavel pela task 9 e 16
+app.post('/task/responsible', async (req: Request, res: Response) => {
+    try {
+        const { taskID, responsibleUserID } = req.body
+        await responsible(taskID, responsibleUserID)
 
-        if(!user || user.length){
+        if (!taskID || taskID.length === 0 || !responsibleUserID) {
             reqError = 402
             res.status(reqError).send('Please, fill the fields')
         }
-        res.status(201).send(result)
-    } catch(err:any){
+
+        for (let i of responsibleUserID) {
+            let responsible_user_id = i
+            await connection('TodoListResponsibleUserTaskRelation').insert({ taskID, responsible_user_id })
+        }
+
+        res.status(201).send('Success')
+    } catch (err: any) {
         res.status(500).send(err.message)
     }
 })
 
 // pesquisar user por ID
-app.get('/user', async (req:Request, res:Response)=>{
-    try{
+app.get('/user', async (req: Request, res: Response) => {
+    try {
         const user = req.body.user as string
         const result = await searchUser(user)
-        if(!user){
+        if (!user) {
             reqError = 402
-            res.status(reqError).send(`User don't exist` )
+            res.status(reqError).send(`User don't exist`)
         }
-        if(result.length === 0){
+        if (result.length === 0) {
             reqError = 404
             res.status(reqError).send([])
-            
+
         }
         res.status(200).send(result)
 
-    } catch(err:any){
+    } catch (err: any) {
         res.status(500).send(err.message)
     }
 })
 
+// pegar user responsavel pela task
 
+app.get('/task/:id/responsible', async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
+        const result = await getResponsibleUser(id)
+
+        if (!id || id.length === 0) {
+            reqError = 401
+            res.status(reqError).send('Please, choose a valid ID')
+        }
+        res.status(201).send(result)
+
+    } catch (err: any) {
+        res.status(500).send(err.message)
+    }
+})
+
+// pegar task por ID
+
+app.get('/task/:id', async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
+        const result = await getTaskByID(id)
+        if (!id) {
+            reqError = 401
+            res.status(reqError).send('Please, choose a valid ID')
+        }
+        res.status(201).send(result)
+    } catch (err: any) {
+        res.status(500).send(err.message)
+    }
+})
+
+// att status da task
+app.put('/task/status/:id/', async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
+        await attTask(id)
+        if (!id) {
+            reqError = 402
+            res.status(reqError).send('Please, put a valid ID')
+        }
+        res.status(201).send('Changed')
+    } catch (err: any) {
+        res.status(500).send(err.message)
+    }
+})
+
+// pegar task por status
+app.get('/tasks/check', async (req: Request, res: Response) => {
+    try {
+        const status = req.query.status as string
+        const result = await getTaskByStatus(status)
+
+        if (!status) {
+            reqError = 402
+            res.status(reqError).send('Please, write doing or to_do')
+        }
+        res.status(201).send(result[0])
+
+    } catch (err: any) {
+        res.status(500).send(err.message)
+    }
+
+})
+
+// pegar task atrasada
+app.get('/tasks/delayed', async (req: Request, res: Response) => {
+    try {
+        const result = await delayedTask()
+        let date = new Date().getTime()
+        const filter = result[0].filter((d: any) => {
+            let datefilter = d.limitDate.getTime()
+            if (date < datefilter) {
+                return d
+            }
+        })
+        res.status(200).send(filter)
+    } catch (err: any) {
+        res.status(500).send(err.message)
+
+    }
+})
+
+// deletar responsavel
+import { connection } from './connection'
+
+app.delete('/task/delete/:taskID/responsible/:responsibleUserId', async (req: Request, res: Response) => {
+    try {
+        const { taskID, responsibleUserId } = req.params
+        if (!taskID || !responsibleUserId) {
+            reqError = 404
+            res.status(reqError).send('Please, check the fields')
+        }
+
+        const taskToRemove = await connection()
+            .select("task_id")
+            .from("TodoListResponsibleUserTaskRelation")
+            .where("responsible_user_id", responsibleUserId)
+
+        if (taskToRemove.length === 0) {
+            reqError = 404
+            res.status(reqError).send('Not found any task with this ID')
+        }
+        const userToRemove = await connection()
+            .select("responsible_user_id")
+            .from("TodoListResponsibleUserTaskRelation")
+            .where("responsible_user_id", responsibleUserId)
+
+        if (userToRemove.length === 0) {
+            reqError = 404
+            res.status(reqError).send(`Don't have users to delete`)
+        }
+
+        await connection.raw(`
+        DELETE FROM TodoListResponsibleUserTaskRelation 
+        WHERE task_id = "${taskToRemove[0].task_id}" AND responsible_user_id = "${userToRemove[0].responsible_user_id}"
+    `)
+        res.status(201).send('Removed from responsible')
+    } catch (err: any) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.get('/tasks/search', async (req:Request, res:Response)=>{
+    try{
+        const srch = req.query.srch as string
+        const result = await searchTask(srch)
+
+        if(!srch){
+            reqError = 401
+            res.status(reqError).send('Please, fill the field')
+        }
+        if(result.length=== 0){
+            reqError = 404
+            res.status(reqError).send('Task not found')
+        }
+        res.status(201).send(result)
+    } catch(err:any){
+        res.status(500).send(err.message)
+
+    }
+}) 
