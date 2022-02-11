b746bbbc96870543f44b087ca1435e5858a0a4df
0dayIg0r <igormedeiros.f@gmail.com>
2022-02-11 16:24:39 -0300

    corrigindo funcao de criar user

--- modulo4/back-labecommerce/src/endpoints/CreateUser.ts
+++ modulo4/back-labecommerce/src/endpoints/CreateUser.ts
@@ -1,16 +1,18 @@
 import {Request, Response} from 'express'
+import { createU } from './SQLFunctions'
 
 let reqError = 0
 export const createUser = async (req:Request, res:Response): Promise<any> =>{
     try{
-        const { name, email, password} = req.body 
+        const { id, name, email, password} = req.body 
         if(!name || !email || !password || password.length === ''){
             reqError = 401
             res.status(reqError).send('Please, verify the fields')
            
         }
-        res.status(200).send('Sucessfull')
-    } catch{
-        res.status(500).send('Internal Server Error')
+        await createU(id, name, email, password)
+        res.status(201).send('Sucessfull')
+    } catch(err:any){
+        res.status(500).send(err.message)
     }
 }
