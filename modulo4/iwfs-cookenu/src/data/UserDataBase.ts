cabdb0b6bb76f58a43db10a4245a597e1f100333
0dayIg0r <igormedeiros.f@gmail.com>
2022-02-24 21:11:23 -0300

    login concluido

--- modulo4/iwfs-cookenu/src/data/UserDataBase.ts
+++ modulo4/iwfs-cookenu/src/data/UserDataBase.ts
@@ -27,5 +27,7 @@ export class UserDataBase extends baseDataBase{
     }
 
 
+
+
 }
 
--- modulo4/iwfs-cookenu/src/index.ts
+++ modulo4/iwfs-cookenu/src/index.ts
@@ -1,8 +1,10 @@
 import  app  from "./app"
+import { login } from "./endpoints/login"
 import { signup } from "./endpoints/signup"
 
 
 app.post('/signup', signup)
+app.post('/login', login)
 
 
 
