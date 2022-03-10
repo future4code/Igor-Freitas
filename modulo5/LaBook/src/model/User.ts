260658aed79201b20df5f977a86ea0bd991aa21f
0dayIg0r <igormedeiros.f@gmail.com>
2022-03-09 21:50:45 -0300

    add type User

--- modulo5/LaBook/src/model/User.ts
+++ modulo5/LaBook/src/model/User.ts
@@ -4,3 +4,26 @@ export type SingupInputDTO = {
     password: string
 }
 
+export class User{
+    constructor(
+        private id: string,
+        private name: string,
+        private email: string,
+        private password: string
+    ){}
+    static toUserModel(data: any): User {
+        return new User(data.id, data.name, data.email, data.password)
+    }
+    public getId(){
+        return this.id
+    }
+    public getName(){
+        return this.name
+    }
+    public getEmail(){
+        return this.email
+    }
+    public getPassword(){
+        return this.password
+    }
+}
