0753b2711668ff66c7038ebcb312e36105753e5b
0dayIg0r <igormedeiros.f@gmail.com>
2022-03-12 14:59:31 -0300

    corrigindo postBusines

--- modulo5/LaBook/src/business/PostBusiness.ts
+++ modulo5/LaBook/src/business/PostBusiness.ts
@@ -2,27 +2,27 @@ import { CreatePostInputDTO, Post } from "../model/Post"
 import { IdGeneration } from "../services/idGenerator"
 import { Authenticator } from "../services/Authenticador"
 import { PostRepository } from "./PostRepository"
+import { PostDatabase } from "../data/PostDatabase"
 
 export class PostBusiness {
-    private postData!: PostRepository
-    private idGenerator!:IdGeneration
-    private authentication!: Authenticator
-
-    construtor(postDataImplementation:PostRepository ){
+    private postData: PostRepository
+    private idGenerator: IdGeneration
+    private authentication: Authenticator
+    constructor(postDataImplementation: PostRepository) {
         this.postData = postDataImplementation
         this.idGenerator = new IdGeneration()
         this.authentication = new Authenticator()
     }
-    createPost = async(input:CreatePostInputDTO, token:string) =>{
-        const {photo, description, type, author_id} = input
+    createPost = async (input: CreatePostInputDTO, token: string) => {
+        const { photo, description, type, author_id } = input
         const id = this.idGenerator.generateId()
-    
-        if(!photo || !description || !type || !author_id){
+
+        if (!photo || !description || !type || !author_id) {
             throw new Error('Verifique se todos os campos estão preenchidos')
         }
 
         const verifyToken = this.authentication.getTokenData(token)
-        if(!verifyToken){
+        if (!verifyToken) {
             throw new Error('Token inválido')
         }
 
@@ -40,18 +40,18 @@ export class PostBusiness {
         await this.postData.insert(post, verifyToken.id)
         return 'Postado'
     }
-    getPostById = async(id:string, token:string) =>{
-        if(!id){
+    getPostById = async (id: string, token: string) => {
+        if (!id) {
             throw new Error('Insira um Id válido')
         }
 
         const verifyToken = this.authentication.getTokenData(token)
-        if(!verifyToken){
+        if (!verifyToken) {
             throw new Error('Token inválido')
         }
 
         const result = await this.postData.getPostById(id)
-        return result 
+        return result
     }
 }
 
