7566308556eda4d825a9e626aa323cf8d4da365c
0dayIg0r <igormedeiros.f@gmail.com>
2022-03-12 14:03:26 -0300

    add função user repository

--- modulo5/LaBook/src/business/UserRepository.ts
+++ modulo5/LaBook/src/business/UserRepository.ts
@@ -3,4 +3,5 @@ import { User } from "../model/User"
 export interface UserRepository{
     insert(user: User):Promise<User>
     findUserByEmail(email: string):Promise<User | null>
+    findUserById(id:string):Promise<User | null>
 }
